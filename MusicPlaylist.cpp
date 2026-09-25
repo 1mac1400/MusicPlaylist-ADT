94775bc5d9965b9d1a9d6d69a9816efe9eb16fac
1mac1400 <167825555+1mac1400@users.noreply.github.com>
2026-09-24 23:11:57 -0700

    Update MusicPlaylist.cpp

--- MusicPlaylist.cpp
+++ MusicPlaylist.cpp
@@ -18,5 +18,112 @@
 
 MusicPlaylist::MusicPlaylist()
 {
+    capacity = 10; //default number of spaces avaliable
+    size = 0;
+    playlist = new std::string[capacity];
     
+    if (playlist == nullptr)
+    {
+        std::cout << "ERROR: Memory Allocation Failed In Constructor\n\n";
+        exit(1);
+    }
+}
+
+
+MusicPlaylist::MusicPlaylist(const MusicPlaylist& otherMusicPlaylist)
+{
+    capacity = otherMusicPlaylist.capacity;
+    size = otherMusicPlaylist.size;
+    
+    playlist = new std::string[capacity];
+    
+    if (playlist == nullptr)
+    {
+        std::cout << "ERROR: Memory Allocation Failed In copy constructor\n\n";
+        exit(1);
+    }
+    
+    for (int i = 0; i < capacity; i++)
+    {
+        playlist[i] = otherMusicPlaylist.playlist[i]; //assigns elements to the new playlist
+    }
+    
+    
+}
+
+MusicPlaylist& MusicPlaylist::operator=(const MusicPlaylist& otherMusicPlaylist)
+{
+    if (this == &otherMusicPlaylist) //checks self assignment
+    {
+        return *this;
+    }
+    
+    delete[] playlist;
+    
+    size = otherMusicPlaylist.size;
+    capacity = otherMusicPlaylist.capacity;
+    
+    playlist = new std::string[capacity];
+    
+    if (playlist == nullptr)
+    {
+        std::cout << "ERROR: Memory Allocation Failed In overloaded assignment operator\n\n";
+        exit(1);
+    }
+    
+    for (int i = 0; i < capacity; i++)
+    {
+        playlist[i] = otherMusicPlaylist.playlist[i]; //assigns elements to the new playlist
+    }
+    
+    return *this;
+    
+}
+
+MusicPlaylist::MusicPlaylist(MusicPlaylist&& otherMusicPlaylist)
+{
+    capacity = otherMusicPlaylist.capacity;
+    size = otherMusicPlaylist.size;
+    
+    playlist = otherMusicPlaylist.playlist; //transfers ownership of the array from otherMusicPlaylist to playlist
+    otherMusicPlaylist.playlist = nullptr; //prevents accidential deletion of playlist array
+    
+}
+
+MusicPlaylist& MusicPlaylist::operator=(MusicPlaylist&& otherMusicPlaylist)
+{
+    if (this == &otherMusicPlaylist) //checks self assignment
+    {
+        return *this;
+    }
+    
+    delete[] playlist;
+    capacity = otherMusicPlaylist.capacity;
+    size = otherMusicPlaylist.size;
+    
+    playlist = otherMusicPlaylist.playlist; //transfers ownership of the array from otherMusicPlaylist to playlist
+    otherMusicPlaylist.playlist = nullptr; //prevents accidential deletion of playlist array
+    
+    return *this;
+}
+
+
+MusicPlaylist::~MusicPlaylist()
+{
+    delete[] playlist;
+    playlist = nullptr;
+    
+}
+
+std::ostream& operator<<(std::ostream& out, const MusicPlaylist& printPlaylist)
+{
+    out << "(";
+    for (int i = 0; i < printPlaylist.size; i++)
+    {
+        out << printPlaylist.playlist[i] << " "; //prints out each song in playlist
+        
+    }
+    out << ")";
+    
+    return out;
 }
